d81ac80bd3abf73af930fd118e06b170b10dafa7
danieledv0 <daniele.dallavecchia@studenti.unitn.it>
2024-12-01 18:52:02 +0100

    added private area and user's creted activities endpoints

--- Backend/app/users.js
+++ Backend/app/users.js
@@ -35,7 +35,18 @@ router.put('/promote/:id', authenticateToken, verifyAdmin, async(req,res) =>{
     }
 });
 
-
+router.get('/private', authenticateToken, async(req, res) =>{
+    try{
+        const userId = req.user.id;
+        const user = await User.findById(userId).select('-password');
+        if(!user){
+            return res.status(404).json({message: 'Utente non trovato'});
+        }
+        res.status(200).json(user);
+    } catch(error){
+        res.status(500).json({message: 'Errore durante il recupero dei dati utente', error});
+    }
+});
 
 
 module.exports = router;
