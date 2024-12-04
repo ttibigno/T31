5fb46443b35e46fc8b3262f580647fe64fad99f6
FrBgnTN <francesco.bignotti@studenti.unitn.it>
2024-12-04 15:37:09 +0100

    Added Id Checks

--- Backend/app/security/checks.js
+++ Backend/app/security/checks.js
@@ -1,4 +1,6 @@
 const Activity = require('../model/activity');
+const mongoose = require('mongoose')
+const {Types: {ObjectId}} = mongoose;
 
 async function userJoined(userId, activityId){
     var activity = await Activity.findById(activityId)
@@ -8,4 +10,17 @@ async function userJoined(userId, activityId){
     return false
 }
 
-module.exports = userJoined
+async function userReported(userId, activityId){
+    var activity = await Activity.findById(activityId)
+    if (activity != undefined && activity.reportUserIds.length > 0) {
+        if (await activity.reportUserIds.includes(userId)) return true
+    }
+    return false
+}
+
+async function isIDValid(anyId){
+    return (ObjectId.isValid(anyId) && (new ObjectId(anyId)).toString === anyId )
+}
+
+
+module.exports = {userJoined, userReported, isIDValid}
