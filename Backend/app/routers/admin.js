57e6251f19467076868b5c9d881394e008939e82
FrBgnTN <francesco.bignotti@studenti.unitn.it>
2025-06-18 12:12:26 +0200

    update user delete

--- Backend/app/routers/admin.js
+++ Backend/app/routers/admin.js
@@ -91,7 +91,7 @@ router.delete('/manageUsers/:id', async(req,res) =>{
         if(admin){
             return res.status(400).json({message: 'Non puoi eliminare un admin'});
         }
-        username = User.findById(userId).select('username');
+        username = await User.findById(userId).select('username');
         await Activity.deleteMany({ creator: username });
         await user.deleteOne()
         res.status(200).json({ message: 'Utente eliminato con successo'});
--- Backend/app/routers/users.js
+++ Backend/app/routers/users.js
@@ -172,7 +172,7 @@ router.put('/private', async (req, res) => {
         const userId = req.user.id;
         console.log("Tentativo di eliminazione dell'utente con ID:", userId);
 
-        username = User.findById(userId).select('username');
+        username = await User.findById(userId).select('username');
         await Activity.deleteMany({ creator: username });
         await User.findByIdAndDelete(userId);
         res.status(200).json({ message: "Account eliminato con successo." });
